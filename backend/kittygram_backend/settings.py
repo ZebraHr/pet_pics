7bc8a37ec5ca0a00dd24de262ff55fce61a1eb6f
Anna Pobedonostseva <zebra9999@rambler.ru>
2023-08-22 16:17:22 +0300

    fix2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -59,17 +59,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'django'),
-        'USER': os.getenv('POSTGRES_USER', 'django'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', ''),
-        'PORT': os.getenv('DB_PORT', 5432)
-    }
+DATABASES = { 
+    'default': { 
+        'ENGINE': 'django.db.backends.postgresql', 
+        'NAME': os.getenv('POSTGRES_DB', 'kittygram'), 
+        'USER': os.getenv('POSTGRES_USER', 'kittygram_user'), 
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''), 
+        'HOST': os.getenv('DB_HOST', ''), 
+        'PORT': os.getenv('DB_PORT', 5432) 
+    } 
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
